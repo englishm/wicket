3f04573e700cc2faa690071685f0f88ce025061e
Mike English <mike.english@gmail.com>
2023-11-02 02:02:07 -0400

    Start wiring up QUIC server

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 mod cli;
 use clap::Parser;
 use cli::*;
+use std::sync::Arc;
 
 fn main() {
     let _config = Config::parse();
+    let server_crypto_config = rustls::ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(
+            vec![rustls::Certificate(vec![0])],
+            rustls::PrivateKey(vec![0]),
+        )
+        .unwrap();
+    let udp_socket = std::net::UdpSocket::bind("[::]:4420").unwrap();
+    let server_config = quinn::ServerConfig::with_crypto(Arc::new(server_crypto_config));
+    let rt = quinn::default_runtime().unwrap();
+    let endpoint_config = quinn::EndpointConfig::default();
+    let _quic_listener =
+        quinn::Endpoint::new(endpoint_config, Some(server_config), udp_socket, rt).unwrap();
 }
