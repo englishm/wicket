b29a26ce00a28a22aec43f5615d47dccfd6b9146
Mike English <mike.english@gmail.com>
2023-11-02 00:56:42 -0400

    Parse server config param as URL

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
+use url::Url;
 
 #[derive(Parser, Clone, Debug)]
 pub struct Config {
     #[arg(long, default_value = "https://localhost:4443")]
-    pub server: String,
+    pub server: Url,
 }
